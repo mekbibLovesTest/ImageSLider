8867cdf82fbc52a511308df9137a143cc6856afb
kazumaneet <mekbibtare@gmail.com>
2023-10-25 17:02:17 +0300

    Slide images by interval

--- src/images.js
+++ src/images.js
@@ -1,3 +1,5 @@
+
+// eslint-disable-next-line no-undef
 const path = require.context("./images", false, /\.jpg$/);
 const imagesArray = path.keys().map(path);
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 import imagesArray from "./images.js";
 
 let current = 0;
+let forward = true;
 const imageHolder = document.querySelector(".imageHolder");
 imagesArray.forEach((image) => {
   const imageCard = document.createElement("div");
@@ -10,6 +11,8 @@ imagesArray.forEach((image) => {
   imageCard.append(imageElement);
   imageHolder.append(imageCard);
 });
+
+setInterval(slideWhenWhenTimeIsUp, 2000);
 const body = document.querySelector("body");
 const next = document.createElement("button");
 next.textContent = "next";
@@ -19,18 +22,25 @@ const previous = document.createElement("button");
 previous.textContent = "previous";
 previous.addEventListener("click", slideToPrevious);
 
-body.append(next, previous);
+body.append(previous, next, createCircles());
 
 function slideToNext() {
   if (current === imagesArray.length - 1) return;
   slide("-");
+  current = current + 1;
+  changeForward();
 }
 
 function slideToPrevious() {
   if (current <= 0) return;
   slide("+");
+  current = current - 1;
+  changeForward();
 }
-
+function changeForward() {
+  if (current === imagesArray.length - 1) forward = false;
+  if (current === 0) forward = true;
+}
 function slide(op) {
   const imageNodes = document.querySelectorAll("img");
   const windowWidth = window.innerWidth;
@@ -43,8 +53,35 @@ function slide(op) {
       image.style.left = `${
         Number(image.style.left.replace("px", "")) - windowWidth
       }px`;
-    image.style.transition = "left 2s";
+    image.style.transition = "left 1s";
   });
-  if (op === "+") current = current - 1;
-  else current = current + 1;
+}
+
+function createCircles() {
+  const div = document.createElement("div");
+  for (let i = 0; i < imagesArray.length; i++) {
+    let input = document.createElement("input");
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "image");
+    input.setAttribute("image-id", i);
+    input.addEventListener("click", slideToChosen);
+    div.append(input);
+  }
+  return div;
+}
+
+function slideToChosen(e) {
+  const chosen = Number(e.target.getAttribute("image-id"));
+  if (chosen > current) {
+    while (chosen > current) slideToNext();
+  } else if (chosen < current) {
+    while (chosen < current) slideToPrevious();
+  }
+}
+
+function slideWhenWhenTimeIsUp() {
+  if (current < imagesArray.length - 1 && forward) slideToNext();
+  else if (current > 0) slideToPrevious();
+  console.log(current);
+  console.log(forward);
 }
